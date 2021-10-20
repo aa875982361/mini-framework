51963c2748c4f59a1dd7f54fbf6711b4e21026c1
huangpeilin <huangpeilin@qunjielong.com>
2021-10-20 18:07:16 +0800

    feat: 增加逻辑层事件触发逻辑

--- src/logic.ts
+++ src/logic.ts
@@ -37,16 +37,21 @@ console.log("wx.getSystemInfoSync", res);
 const logicAcceptMessageName = "onMyMessage";
 (window as any)[logicAcceptMessageName] = function(data: any){
     console.log("逻辑层接收到数据", data);
+    const { eventName = "" } = data
+    console.log("page 是否存在对应的方法", !!page[eventName]);
+    console.log("event", data.event);
+    
+    page[eventName] && page[eventName](data.event)
 }
 /**
  *  通信层发送数据方法名
  */
-const sendMessageToRenderName = "sendMessageToRender"
+const sendMessageToRenderName = "logicSendMessageToRender"
 /**
  * 发送数据到渲染层
  * @param data 数据
  */
-function setMessageToRender(data: any){
+function sendMessageToRender(data: any){
     /** 发送信息给渲染层的方法名 */
     parent.window[sendMessageToRenderName](data)
 }
@@ -55,5 +60,23 @@ function setMessageToRender(data: any){
  * 小程序初始化方法
  */
 function App(AppConfig: any){
-    
+
 }
+
+// 引入某个页面的逻辑
+const page: any = {
+    setData: function(data: any, callback?: () => {}) {
+        console.log("page setdata", data);
+        sendMessageToRender(data)
+        callback && callback()
+    },
+    onLoad: function(): void{
+        console.log("page onload");
+        const renderPage = require("./render/render.js")
+        console.log("renderPage", renderPage);
+        renderPage.run({page})
+    }
+}
+
+page.onLoad()
+
