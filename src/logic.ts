7a4f96256c5c1d3f9fb91f6f5785a730dc2f4c9a
huangpeilin <huangpeilin@qunjielong.com>
2021-10-20 11:11:50 +0800

    feat: 引入微信能力，wx.getSystemInfo等

--- src/logic.ts
+++ src/logic.ts
@@ -2,10 +2,35 @@
  * 逻辑层
  */
 console.log("逻辑层");
+declare var wx: any
 
 // 引入微信的能力模拟实现 taro-h5
-// import Taro from "@tarojs/taro-h5"
-// console.log("taro", Taro);
+(window as any).process = {
+    env: {
+        TARO_ENV: "devlopment"
+    }
+};
+const globalObj: any = {
+    ENABLE_INNER_HTML: true,
+    ENABLE_ADJACENT_HTML: true,
+    ENABLE_SIZE_APIS: true,
+    ENABLE_TEMPLATE_CONTENT: true,
+    ENABLE_CLONE_NODE: true,
+};
+Object.keys(globalObj).map(key => {
+    (window as any)[key] = globalObj[key]
+})
+
+const Taro = require("@tarojs/taro-h5")
+console.log("taro", Taro);
+// Taro.getSystemInfo = Taro.getSystemInfoSync
+(window as any).wx = Taro
+// 小程序里getSystemInfo 是同步的
+wx.getSystemInfo = Taro.getSystemInfoSync.bind(Taro)
+console.log("taro.getSystemInfo", Taro.getSystemInfo());
+const res = wx.getSystemInfo()
+console.log("wx.getSystemInfoSync", res);
+
 
 
 // 子窗口的函数名
@@ -13,8 +38,22 @@ const logicAcceptMessageName = "onMyMessage";
 (window as any)[logicAcceptMessageName] = function(data: any){
     console.log("逻辑层接收到数据", data);
 }
-/** 发送信息给渲染层的方法名 */
+/**
+ *  通信层发送数据方法名
+ */
 const sendMessageToRenderName = "sendMessageToRender"
-parent.window[sendMessageToRenderName]("我是逻辑层的数据")
+/**
+ * 发送数据到渲染层
+ * @param data 数据
+ */
+function setMessageToRender(data: any){
+    /** 发送信息给渲染层的方法名 */
+    parent.window[sendMessageToRenderName](data)
+}
 
-
+/**
+ * 小程序初始化方法
+ */
+function App(AppConfig: any){
+    
+}
